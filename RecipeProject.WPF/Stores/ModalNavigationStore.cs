4776a27190da20a236765f83ecc9ac58acc91aee
User <User@DESKTOP-1HBE7CA>
2022-11-11 06:55:00 -0800

    Add project files.

---
+++ RecipeProject.WPF/Stores/ModalNavigationStore.cs
@@ -0,0 +1,31 @@
+﻿using RecipeProject.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeProject.WPF.Stores
+{
+    public class ModalNavigationStore
+    {
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set
+            {
+                _currentViewModel = value;
+                CurrentViewModelChanged?.Invoke();
+            }
+        }
+        public bool IsModalOpen => CurrentViewModel != null;
+
+        public Action CurrentViewModelChanged;
+
+        public void Close()
+        {
+            CurrentViewModel = null;
+        }
+    }
+}
---
+++ RecipeProject.WPF/ViewModels/LoginViewModel.cs
@@ -0,0 +1,91 @@
+﻿using RecipeProject.WPF.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RecipeProject.WPF.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+        public string Username
+        {
+            get { return _username; }
+            set 
+            { 
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        public SecureString Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        public bool IsViewVisible
+        {
+            get { return _isViewVisible; }
+            set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+
+        public ICommand SignInCommand { get; }
+        public ICommand RecoveryPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+        public LoginViewModel()
+        {
+            SignInCommand = new ViewModelCommand(ExecuteLoginCommand, CandExecuteLoginCommand);
+            RecoveryPasswordCommand = new ViewModelCommand(ExecuteRecoverCommand);
+        }
+
+        private void ExecuteRecoverCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CandExecuteLoginCommand(object obj)
+        {
+            bool validData;
+            if (string.IsNullOrEmpty(Username) || Username.Length < 3 || Password == null || Password.Length < 3)
+            {
+                validData= false;
+            }
+            else
+            {
+                validData = true;
+            }
+
+            return validData;
+        }
+    }
+}
