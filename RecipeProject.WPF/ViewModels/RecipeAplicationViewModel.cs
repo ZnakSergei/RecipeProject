7338790b7c3c2ef9f77be646544f5c62fee65997
User <User@DESKTOP-1HBE7CA>
2022-11-28 06:30:32 -0800

    Views

--- RecipeProject.WPF/ViewModels/RecipeAplicationViewModel.cs
+++ RecipeProject.WPF/ViewModels/RecipeAplicationViewModel.cs
@@ -1,4 +1,5 @@
-﻿using RecipeProject.WPF.Stores;
+﻿using RecipeProject.WPF.Commands;
+using RecipeProject.WPF.Stores;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,13 +16,18 @@ namespace RecipeProject.WPF.ViewModels
         public RecipeDetailsViewModel RecipeDetailsViewModel { get; set; }
         public string Username { get; set; }
         public ICommand AddNewRecipeCommand { get; }
-        public ICommand AddRecipeCommand { get; }
         public ICommand EditUserCommand { get; }
         public ICommand LogOutCommand { get; }
         public RecipeAplicationViewModel(SelectedRecipeStore selectedRecipeStore)
         {
             RecipeListingViewModel= new RecipeListingViewModel(selectedRecipeStore);
             RecipeDetailsViewModel = new RecipeDetailsViewModel(selectedRecipeStore);
+            AddNewRecipeCommand = new ViewModelCommand(ExecuteAddRecipeCommand);
+        }
+
+        private void ExecuteAddRecipeCommand(object obj)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ RecipeProject.WPF/ViewModels/RecipeDetailsFormViewModel.cs
@@ -0,0 +1,73 @@
+﻿using RecipeProject.WPF.Enums;
+using RecipeProject.WPF.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RecipeProject.WPF.ViewModels
+{
+    public class RecipeDetailsFormViewModel : ViewModelBase
+    {
+        private ModalNavigationStore _modalNavigationStore;
+        public ViewModelBase CurrentViewModel => _modalNavigationStore.CurrentViewModel;
+        public bool IsModalOpen => _modalNavigationStore.IsModalOpen;
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set 
+            { 
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+        private string _imagePath;
+        public string ImagePath
+        {
+            get { return _imagePath; }
+            set
+            {
+                _imagePath = value;
+                OnPropertyChanged(nameof(ImagePath));
+            }
+        }
+        private Meal _meal;
+        public Meal Meal
+        {
+            get { return _meal; }
+            set
+            {
+                _meal = value;
+                OnPropertyChanged(nameof(Meal));
+            }
+        }
+        public ICommand SubmitCommand { get; }
+        public ICommand CancelCommand { get; }
+        public RecipeDetailsFormViewModel(ModalNavigationStore modalNavigationStore)
+        {
+            _modalNavigationStore = modalNavigationStore;
+            _modalNavigationStore.CurrentViewModelChanged += _modalNavigationStore_CurrentViewModalChanged;
+        }
+
+        private void _modalNavigationStore_CurrentViewModalChanged()
+        {
+            OnPropertyChanged(nameof(CurrentViewModel));
+            OnPropertyChanged(nameof(IsModalOpen));
+        }
+    }
+}
